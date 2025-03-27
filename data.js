ed61d2a04146f7d1faf589d1bfec95bd0472e6c0
wewman222 <77263223+wewman222@users.noreply.github.com>
2025-03-27 08:34:05 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,35 @@
 
 const firstnames = ["John","Smith","Dave","Harry","Amy","Sue","Claire"]
 const lastnames = ["Smith","Johnson","Williams","Brown",]
-const firstnameoutput = firstnames[Math.random()*firstnames.length | 0]
-const lastnameoutput = lastnames[Math.random()*lastnames.length | 0]
-
-   
+const data = []
+const ul = document.querySelector("ul")
 
 
-function namegenerator(){
-    const firstrandom = Math.floor(Math.random()*firstnames.length);
-    //const lastrandom = Math.floor(Math.random()*lastnames.length);
-    //ul.innerHTML = html.join(firstnames[firstrandom], lastnames[lastrandom]);
-    console.log("Your random person is", firstnames[firstrandom]);
-    document.getElementById("name").innerHTML = firstnames[firstrandom];
+//function namegenerator(){ //OLD GENERATOR, KEEP FOR REFERENCE\\
+//    const firstrandom = Math.floor(Math.random()*firstnames.length);
+//    const lastrandom = Math.floor(Math.random()*lastnames.length);
+//    console.log("Your random person is", firstnames[firstrandom], lastnames[lastrandom]);
+//    const finalname = [firstnames[firstrandom],  lastnames[lastrandom]];
+//    finalname.push
+//    ul.innerHTML = finalname.join(" ")
+//}
+
+function rendername(){
+    const html = data.map((String) => {
+        return `<li>${String}</li>`
+    })
+    console.log(html.join(' '))
+    ul.innerHTML = html.join(' ')
 }
 
-
-
-setInterval(() => {
-    namegenerator()
+const i = setInterval(() => {
+    const firstrandom = Math.floor(Math.random()*firstnames.length);
+    const lastrandom = Math.floor(Math.random()*lastnames.length);
+    console.log("Your random person is", firstnames[firstrandom], lastnames[lastrandom]);
+    const finalname = [firstnames[firstrandom], lastnames[lastrandom]];
+    data.push(finalname)
+    rendername()
+    if(data.length === 5){
+        data.shift()
+    }
 }, 1000)
