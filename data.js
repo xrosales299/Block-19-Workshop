57dbf11a655e0b92df490e7b055841793fd01ca8
wewman222 <77263223+wewman222@users.noreply.github.com>
2025-03-26 21:51:37 -0700

    updating name generator

--- data.js
+++ data.js
@@ -3,15 +3,8 @@ const firstnames = ["John","Smith","Dave","Harry","Amy","Sue","Claire"]
 const lastnames = ["Smith","Johnson","Williams","Brown",]
 const firstnameoutput = firstnames[Math.random()*firstnames.length | 0]
 const lastnameoutput = lastnames[Math.random()*lastnames.length | 0]
-const ul = document.querySelector("ul")
 
-function render(){
-    const html = firstnames.map((string) => {
-        return `<li>${string}</li>`
-    })
-    console.log(html.join(""))
-    ul.innerHTML = html.join("")
-}
+   
 
 
 function namegenerator(){
@@ -19,12 +12,11 @@ function namegenerator(){
     //const lastrandom = Math.floor(Math.random()*lastnames.length);
     //ul.innerHTML = html.join(firstnames[firstrandom], lastnames[lastrandom]);
     console.log("Your random person is", firstnames[firstrandom]);
+    document.getElementById("name").innerHTML = firstnames[firstrandom];
 }
 
 
 
 setInterval(() => {
-    const 
     namegenerator()
-    render()
 }, 1000)
